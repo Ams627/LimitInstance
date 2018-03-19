0cf6528dcf242616a7a0ed01db321ebd806f840e
Adrian Sims <adriansims62781@gmail.com>
2018-03-19 10:06:33 +0000

    Add project files.

---
+++ LimitInstance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitInstance
+{
+    class LimitedInstances<T>
+    {
+        private Stack<T> stack;
+
+        public LimitedInstances(int limit)
+        {
+            stack = new Stack<T>();
+        }
+
+        public T GetInstance()
+        {
+            var t = stack.Pop();
+            return t;
+        }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+    //startstarttypingtypingherehere
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add LimitInstance/Program.cs
+git add LimitInstance/App.config
+git add LimitInstance/Properties/AssemblyInfo.cs
+git add LimitInstance/LimitInstance.csproj
+git add LimitInstance.sln
+git add .gitignore
